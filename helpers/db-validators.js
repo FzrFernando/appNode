9b4bef72c6b9af00fe3d5aef9d37bb3ac76ce33f
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2024-02-07 20:47:39 +0100

    Fallo

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -14,19 +14,4 @@ const existsNick = async (login) => {
     }
 }
 
-const rolValid = async(rol)=>{
-    if(rol!=='ADMIN_ROLE'&&rol!=='USER_ROLE'){
-        throw new Error(`Solo se admite de rol ADMIN_ROLE y USER_ROLE`)
-    }
-}
-
-const isAdmin =  ()=>{
-    const userLog = req.userLogin
-    if(userLog.rol !='ADMIN_ROLE'){
-        throw new Error(`El usuario no es administrador para realizar la acción`)
-
-    }
-   
-}
-
-module.exports = { existsEmail, existsNick, rolValid ,isAdmin }
+module.exports = { existsEmail, existsNick }
